a2748b13e061e6d24fec9482ff301b0e32c0df0e
Chongwei <lewgreat@gmail.com>
2016-04-25 23:55:47 +0800

    Still working on lesson 2.

--- calculator16/Calculator16/Calculator16/CalculatorBrain.swift
+++ calculator16/Calculator16/Calculator16/CalculatorBrain.swift
@@ -18,15 +18,15 @@ class CalculatorBrain{
     }
     
     var operations: Dictionary<String, Operation> = [
-        "π": Operation.Constant,
-        "e": Operation.Constant
+        "π": Operation.Constant(M_PI),
+        "e": Operation.Constant(M_E)
     ]
     
 
     
     enum Operation {
-        case Constant
-        case UnaryOperation
+        case Constant(Double)
+        case UnaryOperation(Double -> Double)
         case BinaryOperation
         case Equals
     }
@@ -34,10 +34,10 @@ class CalculatorBrain{
     func performOperation(symbol: String) {
         if let operation = operations[symbol]{
             switch operation {
-            case .Constant:
-                
+            case .Constant(let value): accumulator = value
+            
             default:
-                <#code#>
+                break
             }
         }
     }
